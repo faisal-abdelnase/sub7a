790a8ccea9bc51d6d7e229e575602b96b800621c
faisal <faisalabdalnasser275@gmail.com>
2024-07-31 23:21:30 +0300

    add const

--- lib/Features/splash/presentation/view/splash_page.dart
+++ lib/Features/splash/presentation/view/splash_page.dart
@@ -15,7 +15,7 @@ class _SplashPageState extends State<SplashPage> {
   @override
   void initState() {
     super.initState();
-    Timer(Duration(seconds: 3), () {
+    Timer(const Duration(seconds: 3), () {
       Navigator.of(context).pushReplacement(
         MaterialPageRoute(builder: (context) => const HomePage ()),
       );
