7bdfb1751bf80a33854267388ebc187337d16683
faisal <faislmahran@gmail.com>
2024-07-31 22:56:09 +0300

    add splash Page

--- lib/Features/splash/presentation/view/splash_page.dart
+++ lib/Features/splash/presentation/view/splash_page.dart
@@ -1,12 +1,31 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-class SplashPage extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:sub7a/Features/sub7a/presentation/view/home_page.dart';
+
+class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 3), () {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => const HomePage ()),
+      );
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: Column(),
     );
   }
 }
