81f993091e925b54fc6cb7904090d680b2f629c2
faisal <faisalabdalnasser275@gmail.com>
2024-08-01 20:42:20 +0300

    add appber and create count page body file

--- lib/Features/count/presentation/view/count_page.dart
+++ lib/Features/count/presentation/view/count_page.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:sub7a/Features/count/presentation/view/widgets/count_page_body.dart';
+import 'package:sub7a/core/utils/colors.dart';
 
 class CountPage extends StatelessWidget {
   const CountPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: red,
+        leading: 
+          IconButton(
+            onPressed: (){}, 
+            icon: const Icon(
+              Icons.color_lens,
+              color: Colors.white,
+              )),
+        
+      ),
+
+      body: const CountPageBody(),
+    );
   }
 }
---
+++ lib/Features/count/presentation/view/widgets/count_page_body.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CountPageBody extends StatelessWidget {
+  const CountPageBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
